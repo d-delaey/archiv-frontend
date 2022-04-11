cd0b02359467e1ea73579233ff79dabf42fb729d
Maxi Quoß <maxi@quoss.org>
2022-04-11 02:30:19 +0200

    api url for downloads

--- src/routes/clips/watch/[uuid].svelte
+++ src/routes/clips/watch/[uuid].svelte
@@ -127,7 +127,7 @@
                                         >
                                             Mehr anzeigen
                                         </button>
-                                        <a href="/dl/clips/{clip.uuid}" class="btn btn-sm" rel="external">
+                                        <a href="{BASE_URL}/dl/clips/{clip.uuid}" class="btn btn-sm" rel="external">
                                             <svg
                                                 xmlns="http://www.w3.org/2000/svg"
                                                 width="16"
--- src/routes/vods/watch/[uuid].svelte
+++ src/routes/vods/watch/[uuid].svelte
@@ -104,7 +104,7 @@
                         >
                             Mehr anzeigen
                         </button>
-                        <a href="/dl/vods/{vod.uuid}" class="btn btn-sm" rel="external">
+                        <a href="{BASE_URL}/dl/vods/{vod.uuid}" class="btn btn-sm" rel="external">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="16"
