263541115ffefac7f26a8f233c3cf7c148cd2400
Maxi Quoß <maxi@quoss.org>
2022-04-11 14:35:13 +0200

    try fix wrong theme for prefered light

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -32,6 +32,10 @@
     <script data-cfasync="false">
         if (document) {
             let theme = localStorage.getItem('theme');
+            if (!theme) {
+                theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+                localStorage.setItem('theme', theme);
+            }
             document.documentElement.setAttribute('data-theme', theme);
         }
     </script>
