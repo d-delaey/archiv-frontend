b2b6caf63480fa12698acd6e3bafd4d181eb6cf5
Maxi Quoß <maxi@quoss.org>
2022-04-17 13:02:06 +0200

    add prettier pre-commit hook
    https://prettier.io/docs/en/precommit.html

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,18 +1,18 @@
 <script context="module">
-  /** @type {import('@sveltejs/kit').ErrorLoad} */  
-  export function load({ error, status }) {
-    return {
-      props: {
-        //title: `${status}: ${error.message}`
-        status: status,
-        message: error.message
-      }
-    };
-  }
+    /** @type {import('@sveltejs/kit').ErrorLoad} */
+    export function load({ error, status }) {
+        return {
+            props: {
+                //title: `${status}: ${error.message}`
+                status: status,
+                message: error.message
+            }
+        };
+    }
 </script>
 
 <script>
-  export let status, message;
+    export let status, message;
 </script>
 
 <h1 class="text-center">{status}</h1>
