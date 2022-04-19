d3978bc3eebfc42cea2d8c3371c5e634f5aec514
Delaey <svenschroeder1998@gmail.com>
2022-04-19 18:50:53 +0200

    added emote in title on year-overview page

--- src/routes/vods/years.svelte
+++ src/routes/vods/years.svelte
@@ -25,6 +25,14 @@
         vods[y] = year.results;
     }
 
+    async function fetchEmotes(provider) {
+        const response = await fetch(
+            `${import.meta.env.VITE_BASE_URL}/emotes/?page_size=500&provider=${provider}`
+        );
+        const e = await response.json();
+        return e;
+    }
+
     function toHHMMSS(duration) {
         const sec_num = parseInt(duration, 10);
         let hours = Math.floor(sec_num / 3600);
@@ -34,6 +42,32 @@
         let m = minutes < 10 ? '0' + minutes : minutes;
         let s = seconds < 10 ? '0' + seconds : seconds;
         return h + ':' + m + ':' + s;
+    }
+    async function showEmotesInTitle(title) {
+        let emotes = (await fetchEmotes('ffz')).results;
+        let newTitle = title;
+        let emoteNames = [];
+        let emoteLinks = [];
+
+        Object.keys(emotes).map((key) => {
+            emoteNames.push(emotes[key].name);
+            emoteLinks.push(emotes[key].url);
+        });
+
+        for (let i = 0; i < emoteNames.length; i++) {
+            let value = emoteNames[i];
+            let imgHTML =
+                '<img src="' +
+                emoteLinks[i] +
+                '" alt="' +
+                value +
+                '" loading="lazy" height="20" data-toggle="tooltip" title=' +
+                value +
+                '/>';
+            newTitle = newTitle.replace(value, imgHTML);
+        }
+
+        return newTitle;
     }
 </script>
 
@@ -83,7 +117,11 @@
                                             </div>
                                             <div class="col order-first order-md-last fw-bold">
                                                 <a href="/vods/watch/{vod.uuid}">
-                                                    {vod.title}
+                                                    {#await showEmotesInTitle(vod.title)}
+                                                        {vod.title}
+                                                    {:then newTitle}
+                                                        {@html newTitle}
+                                                    {/await}
                                                 </a>
                                             </div>
                                             <div class="col-md-auto order-sm-last">
